292aab12d1dc72c0f94b5391380952ab2b97664c
Avnish327030 <anishyadav7045075175@gmail.com>
2021-03-24 22:33:04 +0530

    everything is setup to deploy now

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return "<h1>This one is avnish yadav for ci cd demo</h1>"
+
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -11,6 +21,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
