b472c2501dde6467d02c2d5f5bf4b493005c0b74
Avnish327030 <anishyadav7045075175@gmail.com>
2021-03-24 22:36:18 +0530

    modified\

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return "<h1>This one is avnish yadav for ci cd demo</h1>"
+    return "<h1>This  is avnish yadav for ci cd demo</h1>"
 
 
 # This is a sample Python script.
